7d6aa71379aab93f9eab493ea2df3f22e76d82f3
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2021-03-24 17:44:08 +0900

    release

--- server.go
+++ server.go
@@ -3,21 +3,23 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
 	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 // Msg is request message from client
 type Msg struct {
-	DirPath      string   `json:"dir_path" form:"dir_path" query:"dir_path"`
-	ExcludeFiles []string `json:"exclude_files" form:"exclude_files" query:"exclude_files"`
-	IsRecursive  bool     `json:"is_recursive" form:"is_recursive" query:"is_recursive"`
+	DirPath               string   `json:"dir_path" form:"dir_path" query:"dir_path"`
+	ExcludeFiles          []string `json:"exclude_files" form:"exclude_files" query:"exclude_files"`
+	ExcludeFileExtensions []string `json:"exclude_file_extensions" from:"exclude_file_extensions" query:"exclude_file_extensions"`
+	IsRecursive           bool     `json:"is_recursive" form:"is_recursive" query:"is_recursive"`
 }
 
 // EchoServer is API server
@@ -71,7 +73,7 @@ func generateSet(keys []string) map[string]struct{} {
 	return sets
 }
 
-func deleteFiles(dirPath string, excludeFiles map[string]struct{}, isRecursive bool) error {
+func deleteFiles(dirPath string, excludeFiles map[string]struct{}, excludeFileExtensions map[string]struct{}, isRecursive bool) error {
 	files, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		return err
@@ -80,9 +82,11 @@ func deleteFiles(dirPath string, excludeFiles map[string]struct{}, isRecursive b
 		fmt.Println("find file: " + file.Name())
 		if file.IsDir() {
 			if isRecursive {
-				deleteFiles(filepath.Join(dirPath, file.Name()), excludeFiles, isRecursive)
+				deleteFiles(filepath.Join(dirPath, file.Name()), excludeFiles, excludeFileExtensions, isRecursive)
 			}
-		} else if _, ignore := excludeFiles[file.Name()]; ignore == true {
+		} else if _, ignore := excludeFiles[file.Name()]; ignore {
+			fmt.Println("ignore file: " + file.Name())
+		} else if _, ignore := excludeFileExtensions[filepath.Ext(file.Name())]; ignore {
 			fmt.Println("ignore file: " + file.Name())
 		} else {
 			deleteFile(filepath.Join(dirPath, file.Name()))
@@ -105,6 +109,6 @@ func sweep(c echo.Context) error {
 		fmt.Println("cannot bind data")
 		return c.String(http.StatusBadRequest, "invalid data format")
 	}
-	deleteFiles(m.DirPath, generateSet(m.ExcludeFiles), m.IsRecursive)
+	deleteFiles(m.DirPath, generateSet(m.ExcludeFiles), generateSet(m.ExcludeFileExtensions), m.IsRecursive)
 	return c.String(http.StatusOK, "delete files")
 }
